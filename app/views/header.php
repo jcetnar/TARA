7c5bffa66e736181d9da1b8f5b8e5ca19c6a1787
Jessica Cetnar <jessicacetnar@gmail.com>
2017-01-05 20:42:40 -0500

    new style

--- app/views/header.php
+++ app/views/header.php
@@ -1,4 +1,24 @@
-<ul class='nav nav-pills'> <!-- unordered list, list item -->
+<nav class='navbar navbar-default">
+	<div class="container-fluid">
+	<div class="navbar-header">
+		<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-espanded="false">
+			<span class="sr-only">Toggle navigation</span?
+			<span class="icon-bar"></span>
+			<span class="icon-bar"></span>
+			<span class="icon-bar"></span>
+		</button>
+		<a class="navbar-brand" href="#">TARA</a>
+	</div>
+	<div class="collapse navbar-collapse" id="bs-example-navbar=collapse-1">
+		<ul class="nav navbar-nav">
+			<li class="active"><a href="#">Status <span class="sr-only"> (current)</span></a></li>
+			<li><a href="/immediate">Request Immediate Task</a></li>
+		</ul>
+	</div>
+	</div>
+</nav>
+<!--
+<ul class='nav nav-pills'> 
 		<li role ="presentation" class="active">
 			<a href="/status">Status
 			<!--   <button type="button" class="btn btn-default navbar-btn">Status</button> -->
@@ -9,7 +29,7 @@
 				Immediate Task
 			</a>
 		</li>
-<!-- if ($_SESSION['user_admin'] ==0 { include ("views/user.php);} if ($_SESSION['user_admin'] ==1) { include("views/admin.php");
+<!-- if ($_SESSION['user_admin'] ==0 { include ("views/user.php);} if ($_SESSION['user_admin'] ==1) { include("views/admin.php"); -->
 	<?php if ($isadmin): ?>
 		<li role="presentation">
 			<a href='/items'>
@@ -35,11 +55,10 @@
 	<?php else: ?>
 		<li role="presentation">
 			<!-- absolute link has a / -->
-		<!-- <a href='/login'>
-				Login -->
-			<a href='/user_registration'>
-				User Registration
+		 <a href='/login'>
+				Login
 			</a>
 		</li>
 	<?php endif; ?>
 </ul>
+-->
--- index.php
+++ index.php
@@ -8,7 +8,8 @@ Flight::set('flight.views.path', './app/views');
 //comment 
 
 Flight::route('/', function(){
-    Flight::render('header', array('isadmin' => false), 'header_content');
+  //  Flight::render('header', array('isadmin' => false), 'header_content');
+    Flight::render('header2', array(), 'header_content;);
     Flight::render('homepage', array(), 'body_content');
     Flight::render('footer', array(), 'footer_content');
     Flight::render('layout', array('title' => 'TARA Home Page'));
